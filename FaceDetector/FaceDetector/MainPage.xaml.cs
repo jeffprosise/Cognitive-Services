a5b595a1579e8e7e1f9fedfe592264db6282e645
jeffprosise <jeffpro@wintellect.com>
2020-06-10 10:23:28 -0400

    Updated FaceDetector app

--- FaceDetector/FaceDetector/MainPage.xaml.cs
+++ FaceDetector/FaceDetector/MainPage.xaml.cs
@@ -1,15 +1,10 @@
-﻿using Microsoft.ProjectOxford.Face;
-using Microsoft.ProjectOxford.Face.Contract;
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
@@ -17,12 +12,8 @@ using Windows.UI;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -34,7 +25,7 @@ namespace FaceDetector
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private const string _uri = "BASE_FACE_API_URL";
+        private const string _uri = "FACE_API_URL";
         private const string _key = "SUBSCRIPTION_KEY";
 
         public MainPage()
@@ -73,15 +64,20 @@ namespace FaceDetector
                     Container.UpdateLayout();
                     var ratio = LoadedImage.ActualHeight / image.PixelHeight;
 
+                    Progress.IsActive = true;
+                    Overlay.Visibility = Visibility.Visible;
+
                     try
                     {
-                        Progress.IsActive = true;
-                        Overlay.Visibility = Visibility.Visible;
-
                         // Submit the image to the Face API
-                        var client = new FaceServiceClient(_key, _uri);
+                        FaceClient client = new FaceClient(
+                            new ApiKeyServiceClientCredentials(_key),
+                            new System.Net.Http.DelegatingHandler[] { }
+                        );
 
-                        IEnumerable<FaceAttributeType> attributes = new FaceAttributeType[]
+                        client.Endpoint = _uri;
+
+                        IList<FaceAttributeType> attributes = new FaceAttributeType[]
                         {
                             FaceAttributeType.Gender,
                             FaceAttributeType.Age,
@@ -90,98 +86,74 @@ namespace FaceDetector
                             FaceAttributeType.FacialHair
                         };
 
-                        try
+                        stream.Seek(0L);
+                        var faces = await client.Face.DetectWithStreamAsync(stream.AsStream(), true, false, attributes);
+
+                        Progress.IsActive = false;
+                        Overlay.Visibility = Visibility.Collapsed;
+
+                        foreach (var face in faces)
                         {
-                            stream.Seek(0L);
-                            var faces = await client.DetectAsync(stream.AsStream(), true, false, attributes);
+                            // Highlight the face with a Rectangle
+                            var rect = new Rectangle();
+                            rect.Width = face.FaceRectangle.Width * ratio;
+                            rect.Height = face.FaceRectangle.Height * ratio;
 
-                            Progress.IsActive = false;
-                            Overlay.Visibility = Visibility.Collapsed;
+                            var x = (face.FaceRectangle.Left * ratio) + ((Container.ActualWidth - LoadedImage.ActualWidth) / 2.0);
+                            var y = (face.FaceRectangle.Top * ratio) + ((Container.ActualHeight - LoadedImage.ActualHeight) / 2.0);
+                            rect.Margin = new Thickness(x, y, 0, 0);
+                            rect.HorizontalAlignment = HorizontalAlignment.Left;
+                            rect.VerticalAlignment = VerticalAlignment.Top;
 
-                            foreach (var face in faces)
+                            rect.Fill = new SolidColorBrush(Colors.Transparent);
+                            rect.Stroke = new SolidColorBrush(Colors.Red);
+                            rect.StrokeThickness = 2.0;
+                            rect.Tag = face.FaceId;
+
+                            rect.PointerEntered += (s, args) =>
                             {
-                                // Highlight the face with a Rectangle
-                                var rect = new Rectangle();
-                                rect.Width = face.FaceRectangle.Width * ratio;
-                                rect.Height = face.FaceRectangle.Height * ratio;
+                                // Change the rectangle border to yellow when the pointer enters it
+                                rect.Stroke = new SolidColorBrush(Colors.Yellow);
+                            };
 
-                                var x = (face.FaceRectangle.Left * ratio) + ((Container.ActualWidth - LoadedImage.ActualWidth) / 2.0);
-                                var y = (face.FaceRectangle.Top * ratio) + ((Container.ActualHeight - LoadedImage.ActualHeight) / 2.0);
-                                rect.Margin = new Thickness(x, y, 0, 0);
-                                rect.HorizontalAlignment = HorizontalAlignment.Left;
-                                rect.VerticalAlignment = VerticalAlignment.Top;
-
-                                rect.Fill = new SolidColorBrush(Colors.Transparent);
+                            rect.PointerExited += (s, args) =>
+                            {
+                                // Change the rectangle border to red when the pointer exits it
                                 rect.Stroke = new SolidColorBrush(Colors.Red);
-                                rect.StrokeThickness = 2.0;
-                                rect.Tag = face.FaceId;
+                            };
 
-                                rect.PointerEntered += (s, args) =>
-                                {
-                                    // Change the rectangle border to yellow when the pointer enters it
-                                    rect.Stroke = new SolidColorBrush(Colors.Yellow);
-                                };
+                            rect.PointerPressed += async (s, args) =>
+                            {
+                                // Display information about a face when it is clicked
+                                var id = (Guid)((Rectangle)s).Tag;
+                                var selected = faces.Where(f => f.FaceId == id).First();
 
-                                rect.PointerExited += (s, args) =>
-                                {
-                                    // Change the rectangle border to red when the pointer exits it
-                                    rect.Stroke = new SolidColorBrush(Colors.Red);
-                                };
+                                var gender = selected.FaceAttributes.Gender;
+                                var age = selected.FaceAttributes.Age;
+                                var beard = selected.FaceAttributes.FacialHair.Beard > 0.50 ? "Yes" : "No";
+                                var moustache = selected.FaceAttributes.FacialHair.Moustache > 0.50 ? "Yes" : "No";
+                                var glasses = selected.FaceAttributes.Glasses;
 
-                                rect.PointerPressed += async (s, args) =>
-                                {
-                                    // Display information about a face when it is clicked
-                                    var id = (Guid)((Rectangle)s).Tag;
-                                    var selected = faces.Where(f => f.FaceId == id).First();
-
-                                    var gender = selected.FaceAttributes.Gender;
-                                    var age = selected.FaceAttributes.Age;
-                                    var beard = selected.FaceAttributes.FacialHair.Beard > 0.50 ? "Yes" : "No";
-                                    var moustache = selected.FaceAttributes.FacialHair.Moustache > 0.50 ? "Yes" : "No";
-                                    var glasses = selected.FaceAttributes.Glasses;
-
-                                    // Use reflection to enumerate Emotion properties
-                                    var props = selected.FaceAttributes.Emotion.GetType()
-                                        .GetProperties()
-                                        .Where(pi => pi.PropertyType == typeof(float) && pi.GetGetMethod() != null)
-                                        .Select(pi => new
-                                        {
-                                            pi.Name,
-                                            Value = pi.GetGetMethod().Invoke(selected.FaceAttributes.Emotion, null)
-                                        });
-
-                                    // Determine the dominant emotion
-                                    float max = 0.0f;
-                                    var emotion = String.Empty;
-
-                                    foreach(var prop in props)
+                                // Use reflection to enumerate Emotion properties
+                                var props = selected.FaceAttributes.Emotion.GetType()
+                                    .GetProperties()
+                                    .Where(pi => pi.PropertyType == typeof(double) && pi.GetGetMethod() != null)
+                                    .Select(pi => new
                                     {
-                                        if ((float)prop.Value > max)
-                                        {
-                                            max = (float)prop.Value;
-                                            emotion = prop.Name;
-                                        }
-                                    }
+                                        pi.Name,
+                                        Value = (double)pi.GetGetMethod().Invoke(selected.FaceAttributes.Emotion, null)
+                                    });
 
-                                    // Show the results
-                                    var message = $"Gender: {gender}\nAge: {age}\nBeard: { beard}\nMoustache: {moustache}\nGlasses: {glasses}\nEmotion: {emotion}";
-                                    await new MessageDialog(message).ShowAsync();
-                                };
+                                // Determine the dominant emotion
+                                var max = props.Max(p => p.Value);
+                                var emotion = props.Single(p => p.Value == max).Name;
 
-                                Container.Children.Add(rect);
-                            }
-                        }
-                        catch (FaceAPIException fex)
-                        {
-                            Progress.IsActive = false;
-                            Overlay.Visibility = Visibility.Collapsed;
-                            await new MessageDialog(fex.ErrorMessage).ShowAsync();
-                        }
-                        catch (Exception ex)
-                        {
-                            Progress.IsActive = false;
-                            Overlay.Visibility = Visibility.Collapsed;
-                            await new MessageDialog(ex.Message).ShowAsync();
+                                // Show the results
+                                var message = $"Gender: {gender}\nAge: {age}\nBeard: { beard}\nMoustache: {moustache}\nGlasses: {glasses}\nEmotion: {emotion}";
+                                await new MessageDialog(message).ShowAsync();
+                            };
+
+                            Container.Children.Add(rect);
                         }
                     }
                     catch (Exception ex)
